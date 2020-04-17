73787795a3ca6b340ed9bc2ad127a31db2a35259
Harshada Mulye <mulyeharshada@gmail.com>
2020-04-17 13:46:36 +0200

    delete_comment revised

--- src/main/java/se/kth/sda/skeleton/comments/CommentService.java
+++ src/main/java/se/kth/sda/skeleton/comments/CommentService.java
@@ -1,15 +1,18 @@
 package se.kth.sda.skeleton.comments;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    @Autowired
     public CommentRepository commentRepository;
 
 
     public Comment create(Comment comment) {
         return commentRepository.save(comment);
     }
+
     public void deleteById(Long id) { commentRepository.deleteById(id);
     }
 
