60e83a06262291fe83388f45499b1fff46964090
Shivalatha Rachamalla <shivalatha.r@gmail.com>
2020-04-18 19:11:21 +0200

    Page branding

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar({onLogout}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">SDA starter</a>
+            <a className="navbar-brand" href="#"> COVID-19 </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
