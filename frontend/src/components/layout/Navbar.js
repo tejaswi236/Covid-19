fa15ddc9e225c3326b48c9f6f9a4d11e4661a2dc
Harshada Mulye <mulyeharshada@gmail.com>
2020-04-21 10:53:11 +0200

    logo added

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ function Navbar({onLogout}) {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
+            <img src={require("./coronavirus.gif")} alt="covid" width="50" />
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
